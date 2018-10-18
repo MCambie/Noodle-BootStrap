b8cfa55d4da01a0d6ba0a1bb9a4ffc7884cc1dd6
MCambie <cambie.marie@gmail.com>
2018-10-18 13:27:06 +0200

    derriere rectif

--- js/main.js
+++ js/main.js
@@ -4,6 +4,18 @@ if ('serviceWorker' in navigator) {
            .then(function() { console.log('Service Worker Registered'); });
 };
 
+$(document).ready(function(){
+ $('.header').height($(window).height());
+
+ $(".navbar a").click(function(){
+ 	$("body,html").animate({
+ 		scrollTop:$("#" + $(this).data('value')).offset().top
+ 	},1000)
+
+ })
+
+});
+
 function masquer_div(id)
 {
   if (document.getElementById(id).style.display == 'none')
